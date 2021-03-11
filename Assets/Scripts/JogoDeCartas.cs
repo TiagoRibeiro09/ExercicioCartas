722a2e2e9de7208bf0117deca79e4a2143a699a0
TiagoRibeiro09 <80395576+TiagoRibeiro09@users.noreply.github.com>
2021-03-11 17:54:17 +0000

    Initial commit

---
+++ Assets/Scripts/JogoDeCartas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JogoDeCartas : MonoBehaviour
+{
+    string[] Cartavs = new string[] { "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "10 ", "Valete ", "Dama ", "Rei ", "Ás " };
+    string[] CartaSimbolo = new string[] { " Ouro ", " Espadas ", " Copas ", " Paus " };
+    string CartaAAparecer1;
+    string CartaAAparecer2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Intro();
+    }
+
+
+    private void Intro()
+    {
+        Debug.Log("Aperte Espaço para sortear uma carta");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            string CartaAAparecer1 = Cartavs[Random.Range(0, Cartavs.Length)];
+            string CartaAAparecer2 = CartaSimbolo[Random.Range(0, CartaSimbolo.Length)];
+            Debug.Log("A carta que te saiu é  " + CartaAAparecer1 + "de" + CartaAAparecer2);
+            Intro();
+        }
+
+    }
+}
+  
